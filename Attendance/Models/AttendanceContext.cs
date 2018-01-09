c5810d873e28be30368601b40d8edcd72cda94d3
Alexandra Damaschin <damaschin.alexandra.maria@gmail.com>
2018-01-09 16:52:04 +0000

    AttendanceContext added

---
+++ Attendance/Models/AttendanceContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Attendance.Models
+{
+    public class AttendanceContext : DbContext
+    {
+        public AttendanceContext():base("DefaultConnection")
+        {
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Module> Modules { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+    }
+}
