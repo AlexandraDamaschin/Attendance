d5cedb46626668d4585b26d56097b92e882761ca
Alexandra Damaschin <damaschin.alexandra.maria@gmail.com>
2018-01-10 11:24:03 +0000

    Admin and Student roles added

--- Attendance/Migrations/Configuration.cs
+++ Attendance/Migrations/Configuration.cs
@@ -1,18 +1,21 @@
 namespace Attendance.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Attendance.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<Models.ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(Attendance.Models.ApplicationDbContext context)
+        protected override void Seed(Models.ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
@@ -26,8 +29,55 @@ namespace Attendance.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+            var manager =
+              new UserManager<ApplicationUser>(
+                  new UserStore<ApplicationUser>(context));
 
-         
+            var roleManager =
+                new RoleManager<IdentityRole>(
+                    new RoleStore<IdentityRole>(context));
+
+            //create roles
+            roleManager.Create(new IdentityRole { Name = "Admin" });
+            roleManager.Create(new IdentityRole { Name = "Student" });
+
+            //seeding two applicationUsers
+            context.Users.AddOrUpdate(u => u.Email, new ApplicationUser
+            {
+                UserName = "einstein.albert@itsligo.ie",
+                Email = "einstein.albert@itsligo.ie",
+                PasswordHash = new PasswordHasher().HashPassword("ITSligo$1")
+            });
+
+            context.Users.AddOrUpdate(u => u.Email, new ApplicationUser
+            {
+                UserName = "blogs.joe@itsligo.ie",
+                Email = "blogs.joe@itsligo.ie",
+                PasswordHash = new PasswordHasher().HashPassword("ITSligo$2")
+            });
+            context.SaveChanges();
+
+            //asign roles to users
+            ApplicationUser librarian = manager.FindByEmail("einstein.albert@itsligo.ie");
+            if (librarian != null)
+            {
+                manager.AddToRoles(librarian.Id, new string[] { "Admin" });
+            }
+            else
+            {
+                throw new Exception { Source = "Did not find user" };
+            }
+
+            ApplicationUser member = manager.FindByEmail("blogs.joe@itsligo.ie");
+            if (member != null)
+            {
+                manager.AddToRoles(member.Id, new string[] { "Student" });
+            }
+            else
+            {
+                throw new Exception { Source = "Did not find user" };
+            }
+
         }
 
              
